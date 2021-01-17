c93b8623f7b6eba913c2cd7194a0539c7655f12e
svencarstenrasmusen <sven.the.eagle@gmail.com>
2021-01-17 15:59:50 +0100

    added the GraphQL data

--- lib/pages/all_campaigns_page.dart
+++ lib/pages/all_campaigns_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:campaneo_app/widgets/campaign_tile.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../data/campaign_fetch.dart';
 
 class AllCampaignsPage extends StatelessWidget {
   static const String routeName = '/allcampaigns';
@@ -10,7 +13,39 @@ class AllCampaignsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return MediaQuery.removePadding(
+    return Query(
+      options: QueryOptions(
+        documentNode: gql(CampaignFetch.fetchAll),
+      ),
+      // ignore: missing_return
+      builder: (QueryResult result,
+        {VoidCallback refetch, FetchMore fetchMore}) {
+        if (result.hasException) {
+          return Text(result.exception.toString());
+        }
+        if (result.loading) {
+          return Text('Loading');
+        }
+      // ignore: missing_return
+
+        List campaigns = result.data['getCreatedCampaigns2'];
+        return campaigns.isNotEmpty
+            ? Scrollbar(
+          child: GridView.builder(
+            itemCount: campaigns.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 1.85
+            ),
+            itemBuilder: (BuildContext context, int index) =>
+              CampaignTile(campaigns[index]['name']),
+          ),
+        )
+        : Container();
+      }
+    );
+
+    /**return MediaQuery.removePadding(
         context: context,
         removeTop: true,
         child: Scrollbar(
@@ -23,6 +58,6 @@ class AllCampaignsPage extends StatelessWidget {
               itemBuilder: (BuildContext context, int index) => CampaignTile('Verkehrsanalyse in Innsbruck $index')
           ),
         ),
-    );
+    );*/
   }
 }
